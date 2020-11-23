f5b3690b3b5cd695da7646816f2c0475e125c206
Siddhant Tiwary <siddhanttiwary30@gmail.com>
2020-11-24 00:47:29 +0530

    Update content.js

--- content.js
+++ content.js
@@ -13,7 +13,7 @@ chrome.runtime.onMessage.addListener((req, sender, resp) => {
   text=text.replace(/\s+/g, '')
   var response=""
   var url =
-    "https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text="+text+"&token=00912accafac40acafcf5693e9b5f3bf";
+    "https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text="+text+"&token=${{secrets.KEYID}}";
   console.log(url)
   fetch(url)
     .then((res) => res.json())
