4099a9d500556abfaa779773fe517d2c58f3fc58
Siddhant Tiwary <siddhanttiwary30@gmail.com>
2020-11-25 00:48:34 +0530

    typo

--- content.js
+++ content.js
@@ -13,12 +13,7 @@ chrome.runtime.onMessage.addListener((req, sender, resp) => {
   text=text.replace(/\s+/g, '')
   var response=""
   var url =
-<<<<<<< HEAD
     "https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text="+text+"&token=00912accafac40acafcf5693e9b5f3bf";
-=======
-    "https://api.dandelion.eu/datatxt/sent/v1/?lang=en&text="+text+"&token=${{secrets.KEYID}}";
-  console.log(url)
->>>>>>> f5b3690b3b5cd695da7646816f2c0475e125c206
   fetch(url)
     .then((res) => res.json())
     .then((resp) => {
